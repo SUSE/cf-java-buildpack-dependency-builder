95e410757b157af3c2112b383112d948da24990b
Alex Harford <alex.harford@suse.com>
2017-05-18 15:01:19 -0700

    Ensure the source jar isn't picked up

--- redis-store.sh
+++ redis-store.sh
@@ -9,6 +9,7 @@ VERSION=$(cat redis-store-archives/version)
 INDEX_PATH="/redis-store/index.yml"
 UPLOAD_PATH="/redis-store/redis-store-$VERSION.jar"
 
+rm -f redis-store-archives/redis-store-*sources.jar
 transfer_to_s3 "redis-store-archives/redis-store-*.jar" $UPLOAD_PATH
 update_index $INDEX_PATH $VERSION $UPLOAD_PATH
 invalidate_cache $INDEX_PATH $UPLOAD_PATH
